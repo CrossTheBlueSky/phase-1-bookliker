22b378e5fd01f856b4b386230cd76839bf928524
Derek Mercedes <mercedes.derek@gmail.com>
2023-11-08 12:21:26 -0700

    challenge + bonuses complete

--- js/index.js
+++ js/index.js
@@ -1,8 +1,17 @@
-function initialize(){
+let currentBook
+const testUser = {
+    id: 613,
+    username: "test"
+}
+
+function initializeOrUpdate(){
     fetch("http://localhost:3000/books")
     .then(r=>r.json())
     .then((data)=>{
         data.forEach((book)=>{
+            if(!book.liked){
+                book.liked = false
+            }
             generateBook(book)
         })
     })
@@ -21,17 +30,71 @@ function generateBook(book){
 }
 
 function showDetails(book){
+    currentBook = book
+    let likeText
+    book.liked ? likeText = "Unlike" : likeText = "Like"
     const showPanel = document.querySelector("#show-panel")
     showPanel.innerHTML = `
     <img src=${book.img_url} />
     <h5>${book.description}</h5>
     <p>Liked by ${getUsers(book.users)}</p>
+    <button id="like-button">${likeText}</button>
     `
+    document.querySelector("#like-button").addEventListener("click", ()=>{
+
+        if(book.liked){
+            removeLikedUser(book)
+        }else{
+            book.users.push(testUser)
+        addLikedUser(book)
+        }
+        book.liked = !book.liked
+        
+    })
 }
 
 function getUsers(arr){
     let userArr = []
     arr.forEach(e=>userArr.push((" " + e.username)))
-    return userArr.toString()
+    return  userArr.toString()
 }
-initialize()
+
+function addLikedUser(book){
+    const users = book.users
+    const usersObj = {users}
+    fetch(`http://localhost:3000/books/${book.id}`, {
+        method: "PATCH",
+        headers:{
+            "Content-Type" : "application/json"
+
+        },
+        body: JSON.stringify(usersObj)
+    }).then(()=>{
+        showDetails(currentBook)
+    })
+}
+
+function removeLikedUser(book){
+
+    function arrRemover(arr){
+        arr.pop()
+        return arr
+    }
+
+    const users = arrRemover(book.users)
+    const usersObj = {users}
+    currentBook.users = users
+    console.log (usersObj)
+
+    fetch(`http://localhost:3000/books/${book.id}/`, {
+        method: "PATCH",
+        headers:{
+            "Content-Type" : "application/json"
+
+        },
+        body: JSON.stringify(usersObj)
+    }).then(()=>{
+        showDetails(currentBook)
+    })
+}
+initializeOrUpdate()
